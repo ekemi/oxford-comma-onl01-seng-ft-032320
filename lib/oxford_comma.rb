70a9fe8255ddb0883f3b58286e1385af4ec46c1e
edmundo papi <edmundopierre@yahoo.com>
2020-03-26 16:31:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,31 +1,21 @@
 require 'pry'
   def oxford_comma(array)
-      # y=array.join(", ")
-      # binding.pry
-      # x = ((y[-1] << (" and")).split(" ").reverse).join(' ')
-      # y[-1].replace(x)
-      # y.join(",")
+    
       if array.length == 1
-           return array.join
+            return array.join
 
         elsif array.length == 2
-        array.join(" ")
-        #binding.pry
-        x=((array[-1] << (" and")).split(" ").reverse).join(' ')
-        array[-1].replace(x)
-        array.join(" ")
-        #binding.pry
-
-      else
-        array.join(",")
-         x=array[-1].insert(0,"and ")
-        array[-1].replace(x)
-        #binding.pry
-        array.join(", ")
-
-
-
+            array.join(" ")
+        
+            x = ((array[-1] << (" and")).split(" ").reverse).join(' ')
+            array[-1].replace(x)
+            array.join(" ")
+            
+        else
+            array.join(",")
+            x = array[-1].insert(0,"and ")
+            array[-1].replace(x)
+            array.join(", ")
       end
-
 
   end
