b0827771f0b8373b6e7ca9f4a4a94d8d4e70041b
edmundo papi <edmundopierre@yahoo.com>
2020-03-26 16:32:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+require 'pry'
+  def oxford_comma(array)
+    
+      if array.length == 1
+            return array.join
 
-end
+        elsif array.length == 2
+            array.join(" ")
+        
+            x = ((array[-1] << (" and")).split(" ").reverse).join(' ')
+            array[-1].replace(x)
+            array.join(" ")
+            
+        else
+            array.join(",")
+            x = array[-1].insert(0,"and ")
+            array[-1].replace(x)
+            array.join(", ")
+      end
+
+  end
