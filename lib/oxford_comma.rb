162dd1c3e7a12d38ced1b5d7817da7146ff71e18
edmundo papi <edmundopierre@yahoo.com>
2020-03-26 15:51:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ require 'pry'
       # y.join(",")
       if array.length == 1
            return array.join
-           
+
         elsif array.length == 2
         array.join(" ")
         #binding.pry
